d679a5c2def65e1bf21351205e406ff04c1f4f21
Moyasi Ginko <mrsiyam61@gmail.com>
2023-07-26 22:54:19 +0600

    Add alter table and update the schema

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -44,3 +44,31 @@ CREATE TABLE invoices (
     medical_history_id INT,
     PRIMARY KEY (id)
 );
+
+ALTER TABLE medical_histories
+DROP COLUMN patient_id,
+ADD COLUMN patient_id INTEGER UNIQUE REFERENCES patients(id);
+
+ALTER TABLE medical_history_treatments
+DROP COLUMN medical_history_id,
+DROP COLUMN treatment_id,
+ADD COLUMN medical_history_id INTEGER REFERENCES medical_histories(id),
+ADD COLUMN treatment_id INTEGER REFERENCES treatments(id);
+
+ALTER TABLE invoice_items
+DROP COLUMN invoice_id,
+DROP COLUMN treatment_id,
+ADD COLUMN invoice_id INTEGER REFERENCES invoices(id),
+ADD COLUMN treatment_id INTEGER REFERENCES treatments(id);
+
+ALTER TABLE invoices
+DROP COLUMN medical_history_id,
+ADD COLUMN medical_history_id INTEGER UNIQUE REFERENCES medical_histories(id);
+
+CREATE INDEX idx_medical_history_treatments_medical_history_id ON medical_history_treatments (medical_history_id);
+
+CREATE INDEX idx_medical_history_treatments_treatment_id ON medical_history_treatments (treatment_id);
+
+CREATE INDEX idx_invoice_items_invoice_id ON invoice_items (invoice_id);
+
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items (treatment_id);
